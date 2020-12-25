ccf7d68b417a3e6fa3d8266f5e82f679cf78c774
James Ubiarco <jamesubiarco@protonmail.com>
2020-12-24 18:46:52 -0600

    Mejoré la legibilidad del codigo un poco

--- main.cpp
+++ main.cpp
@@ -3,100 +3,28 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	string codigo[13]{              //son los codigos de barras
-            "7501055317875",
-            "7501030462859",
-            "016000506435",
-            "019836103021",
-            "744886239283",
-            "7501055302086",
-            "7501198350432",
-            "7501011147942",
-            "75067656",
-            "7501086801046",
-			"75051938",
-            "724869003520",
-            "7501761865530"
-		};
-	string descripcion[13]          //Es el nombre del articulo
-        {
-            "Fuze Tea Durazno",
-            "galletas principe",
-            "lucky charms treats barra energetica",
-            "Boing de Guayaba",
-            "Agua natural skarch",
-            "coca",
-            "canada dry",
-            "Inspireka",
-            "Trident Azules",
-            "Agua Epura",
-			"Bubbaloo de fresa",
-            "gomitas de llave",
-            "Stila fit"
-        };
-	string cantidad[13]             //Es la cantidad que contiene el producto
-        {
-            "600 ml",
-            "105 gr",
-            "24 gr",
-            "500ml",
-            "1 litro",
-            "500 ml",
-            "600 ml",
-            "45 gr",
-            "6.5 gr",
-            "1 litro",
-            "5.5 gr",
-            "6 gr",
-            "23 gr"
-        };
-	float precio[14]                //El precio del producto
-        {
-            15,
-            15,
-            9,
-            10,
-            10,
-            15,
-            20,
-            18,
-            3,
-            8,
-            3,
-            2,
-            5,
-        };
-    int inventario[13]
-        {
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-            100,
-        };
-    string articulosLlevadosDescripcion[1300]
-		{
-		};
-	string articulosLlevadosCodigo[1300]
-        {
-		};
-	int articulosLlevadosCantidad[1300]
-		{
-		};
-	int articulosLlevadosPrecio[1300]
-		{
-		};
-	int totalArticulos[14]
-		{
-		};
+	string codigo[13] {
+        "7501055317875","7501030462859","016000506435","019836103021","744886239283","7501055302086","7501198350432","7501011147942","75067656","7501086801046","75051938","724869003520","7501761865530"
+    };
+	string descripcion[13] {
+        "Fuze Tea Durazno","galletas principe","lucky charms treats","Boing de Guayaba","Agua natural skarch","Coca Cola","canada dry","Inspireka","Trident Azules","Agua Epura","Bubbaloo de fresa","gomitas de llave","Stila fit"
+    };
+	string cantidad[13] {
+        "600 ml","105 gr","24 gr","500 ml","1 l","500 ml","600 ml","45 gr","6.5 gr","1 l","5.5 gr","6 gr","23 gr"
+    };
+	float precio[13] {
+        15,15,9,10,10,15,20,18,3,8,3,2,5
+    };
+    int inventario[13] {
+        100,100,100,100,100,100,100,100,100,100,100,100,100
+    };
+
+    string articulosLlevadosDescripcion[1300]{};
+	string articulosLlevadosCodigo[1300]{};
+	int articulosLlevadosCantidad[1300]{};
+	int articulosLlevadosPrecio[1300]{};
+	int totalArticulos[13]{};
+
 	int indice;
 	string codigoBarras;
 	int bandera = 15;
@@ -106,61 +34,47 @@ int main(int argc, char** argv) {
 	int totalIngresoCaja = 0;
 	string cualquierTecla;
 
-    do
-        {
-            cout << "Pon el codigo de barras" << endl;
-            cin >> codigoBarras;
-            for (indice = 0; indice <= 12; indice++)
-            {
-                if (codigoBarras == codigo[indice])
-                    {
-                        bandera = indice;
-                        inventario[indice] = inventario[indice] - 1;
-                        totalArticulos[indice] = totalArticulos[indice] + 1;
-                        totalPrecio = precio[bandera] + totalPrecio;
-                        articulosLlevadosDescripcion[indice2] = descripcion[indice];
-                        articulosLlevadosCodigo[indice2] = codigo[indice];
-                        articulosLlevadosCantidad[indice2] = articulosLlevadosCantidad[indice2] + 1;
-                        articulosLlevadosPrecio[indice2] = precio[indice];
-                    	indice2 = indice2 + 1;
-                    }
-                else
-                    {
-                    }
+    do {
+        cout << "Pon el codigo de barras" << endl;
+        cin >> codigoBarras;
+        for (indice = 0; indice <= 12; indice++) {
+            if (codigoBarras == codigo[indice]) {
+                bandera = indice;
+                inventario[indice] = inventario[indice] - 1;
+                totalArticulos[indice] = totalArticulos[indice] + 1;
+                totalPrecio = precio[bandera] + totalPrecio;
+                articulosLlevadosDescripcion[indice2] = descripcion[indice];
+                articulosLlevadosCodigo[indice2] = codigo[indice];
+                articulosLlevadosCantidad[indice2] = articulosLlevadosCantidad[indice2] + 1;
+                articulosLlevadosPrecio[indice2] = precio[indice];
+                indice2 = indice2 + 1;
             }
-            cout << descripcion[bandera] << "  " << cantidad[bandera] << "    $" << precio[bandera] << endl;
-            if(bandera == 15)
-                {
-                    cout << "Producto no encontrado" << endl;
-                }
-            else
-                {
-                }
-            if(codigoBarras == "0")
-                {
-		            for( indice = 0 ; indice <= indice2 - 1; indice++)
-		            {
-		                cout << endl << articulosLlevadosCodigo[indice] << "    " << articulosLlevadosDescripcion[indice]
-                        << "    $" << articulosLlevadosPrecio[indice];
-		            }
-		            cout << endl << "Total: $" << totalPrecio << endl;
-		            totalIngresoCaja = totalPrecio + totalIngresoCaja;
-		            totalPrecio = 0;
-		            indice2 = 0;
-		            cout << endl << "Pulsa cualquier caracter y pulsa enter para continuar";     //Esta parte limpia la pantalla con system(cls)
-		            cin >> cualquierTecla;
-		            system("cls");
-                }
-            else
-                {
-                }
-        } while(codigoBarras != "-1");
-        cout << endl << "Ventas de cada producto: ";
-        for(indice = 0; indice <= 12; indice++)
-            {
-                cout << endl << codigo[indice] << "    " << descripcion[indice] << "  "
-                << cantidad[indice] << "        vendidos:  " << totalArticulos[indice];       //Aquí se escribe el flujo de caja diario
+        }
+        cout << descripcion[bandera] << "\t\t" << cantidad[bandera] << "\t\t$" << precio[bandera] << endl;
+        if(bandera == 15) {
+            cout << "Producto no encontrado" << endl;
+        }
+        if(codigoBarras == "0") {
+            for( indice = 0 ; indice <= indice2 - 1; indice++) {
+                cout << endl << articulosLlevadosCodigo[indice] << "\t" << articulosLlevadosDescripcion[indice] << "\t $" << articulosLlevadosPrecio[indice];
             }
-        cout << endl << endl << "Estos son los ingresos totales de los productos: $" << totalIngresoCaja;
+            cout << endl << "Total: $" << totalPrecio << endl;
+            totalIngresoCaja = totalPrecio + totalIngresoCaja;
+            totalPrecio = 0;
+            indice2 = 0;
+            cout << endl << "Pulsa cualquier caracter y pulsa enter para continuar";     //Esta parte limpia la pantalla con system(cls)
+            cin >> cualquierTecla;
+            system("cls");
+        }
+    } while(codigoBarras != "-1");
+
+    cout << endl << "Ventas de cada producto: ";
+
+    for(indice = 0; indice <= 12; indice++) {
+        cout << endl << codigo[indice] << "\t" << descripcion[indice] << "\t\t" << cantidad[indice] << "\tvendidos: " << totalArticulos[indice];       //Aquï¿½ se escribe el flujo de caja diario
+    }
+
+    cout << endl << endl << "Estos son los ingresos totales de los productos: $" << totalIngresoCaja;
+
     return 0;
 }
